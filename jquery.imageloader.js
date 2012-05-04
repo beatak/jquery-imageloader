25c056b624eab9b5dc8475d3177c643b45746c41
Takashi Mizohata <beatak@gmail.com>
2012-05-03 22:31:35 -0400

    fixed some issues and clean up

--- jquery.imageloader.js
+++ jquery.imageloader.js
@@ -1,6 +1,14 @@
 (
 function($) {
 
+var MILSEC_INTERVAL = 13;
+var DEFAULT_OPTIONS = {
+  selector: '.myimage',
+  dataattr: 'src',
+  background: false,
+  callback: function () {}
+};
+
 /**
  * memory less intensive, supposed to be clean up all afer done,
  * no event attaching image loader.
@@ -9,121 +17,119 @@ function($) {
  * fire error?
  */
 $.fn.imageloader = function (opts) {
-	var namespace = '_' + ('' + (new Date()).valueOf()).slice(-7),
-  q = Queue.getInstance(),
-	DEFAULT_OPTIONS = {
-		selector: '.myimage',
-    dataattr: 'src',
-		background: false,
-		callback: function () {}
-	};
+  var q = Queue.getInstance();
 
-	var init = function (i) {
+  var init = function (i) {
     var self = this;
     var $this = $(this);
     var data = $this.data();
-		var defaults = $.extend({}, DEFAULT_OPTIONS, opts || {});
-		var $elms = $this.find(buildSelector(defaults.selector, defaults.dataattr));
+    var defaults = $.extend({}, DEFAULT_OPTIONS, opts || {});
+    var $elms = $this.find(buildSelector(defaults.selector, defaults.dataattr));
     var len = $elms.length;
+    var ns = '_' + ('' + (new Date()).valueOf()).slice(-7);
 
     data['imageloader-callback'] = defaults.callback;
-    data['imageloader-hasFinished'] = false;
+    data['imageloader-isLoading'] = true;
     data['imageloader-loadedImageCounter'] = 0;
     data['imageloader-length'] = len;
+    data['imageloader-namespace'] = ns;
 
     if (len === 0) {
       finishImageLoad(this);
     }
     else {
-			$elms.each(
-				function (i, elm) {
-          q.add(buildImageLoadFunc(elm, self, namespace, defaults.background));
-				}
-			);
-      // console.log(['we are gonna load ', len, ' image(s) on ', namespace].join(''));
-			$this.on('loadImage.' + namespace, onLoadImage);
+      $elms.each(
+        function (i, elm) {
+          q.add(buildImageLoadFunc(elm, self, ns, defaults.background));
+        }
+      );
+      // console.log(['we are gonna load ', len, ' image(s) on ', ns].join(''));
+      $this.on('loadImage.' + ns, onLoadImage);
       q.run();
-		}
-		return this;
-	};
+    }
+    return this;
+  };
 
-	var onLoadImage = function (ev, elm) {
-    // console.log('onLoadImage', ev, elm);
+  var onLoadImage = function (ev, elm) {
     var parent = ev.currentTarget;
     var data = $(parent).data();
-		if (data['imageloader-hasFinished']) {
-			console.log('onLoadImage: hasFinished but still called?');
-			return;
-		}
+    if (!data['imageloader-isLoading']) {
+      // console.log('onLoadImage: is not loading but still called?');
+      return;
+    }
     ++data['imageloader-loadedImageCounter'];
-		if (data['imageloader-loadedImageCounter'] >= data['imageloader-length']) {
-			finishImageLoad(parent);
-		}
-	};
+    if (data['imageloader-loadedImageCounter'] >= data['imageloader-length']) {
+      finishImageLoad(parent);
+    }
+  };
 
-	var finishImageLoad = function (parent) {
-    // console.log('finishImageLoad', parent);
+  var finishImageLoad = function (parent) {
     var $parent = $(parent);
     var data = $parent.data();
-    data['imageloader-hasFinished'] = true;
-    data['imageloader-callback']();
-		$parent.off('loadImage.' + namespace, onLoadImage);
+    var callback = data['imageloader-callback'];
+    var ns = data['imageloader-namespace'];
+    $parent.off('loadImage.' + ns, onLoadImage);
     delete data['imageloader-callback'];
-    delete data['imageloader-hasFinished'];
+    delete data['imageloader-isLoading'];
     delete data['imageloader-loadedImageCounter'];
     delete data['imageloader-length'];
-	};
+    delete data['imageloader-namespace'];
+    setTimeout(
+      function () {
+        callback();
+      },
+      MILSEC_INTERVAL
+    );
+  };
 
   var buildSelector = function (selector, dataattr) {
     return [selector, '[data-', dataattr, ']'].join('');
   };
 
-	var buildImageLoadFunc = function (elm, parent, namespace, isBg) {
-		var $elm = $(elm),
-		src = $elm.data('src');
+  var buildImageLoadFunc = function (elm, parent, namespace, isBg) {
+    var $elm = $(elm),
+    src = $elm.data('src');
 
-		var onFinishLoagImage = function (ev) {
-			if (ev && ev.type === 'error') {
-				$elm.remove();
-			}
+    var onFinishLoagImage = function (ev) {
+      if (ev && ev.type === 'error') {
+        $elm.remove();
+      }
       // delete attribute
       $elm.removeAttr('data-src');
       $(parent).triggerHandler('loadImage.' + namespace, elm);
-		};
+    };
 
-		return function () {
-			$('<img />')
-				.bind(
-					'error',
-					function (ev) {
-						$(this).unbind('error').unbind('load');
-						onFinishLoagImage(ev);
-					}
-				)
-				.bind(
-					'load', 
-					function(ev) {
-            // console.log('loaded!: ', src);
-						$(this).unbind('error').unbind('load');
-						if (isBg) {
-							$elm.css('background-image', ['url(', src, ')'].join(''));
-						}
-						else {
-							$elm.attr('src', src);
-						}
-						onFinishLoagImage(ev);
-					}
-				)
-				.attr('src', src);
-		};
-	};
+    return function () {
+      $('<img />')
+        .bind(
+          'error',
+          function (ev) {
+            $(this).unbind('error').unbind('load');
+            onFinishLoagImage(ev);
+          }
+        )
+        .bind(
+          'load',
+          function(ev) {
+            $(this).unbind('error').unbind('load');
+            if (isBg) {
+              $elm.css('background-image', ['url(', src, ')'].join(''));
+            }
+            else {
+              $elm.attr('src', src);
+            }
+            onFinishLoagImage(ev);
+          }
+        )
+        .attr('src', src);
+    };
+  };
 
-	return this.each(init);
+  return this.each(init);
 };
 
 // ===================================================================
 
-var MILSEC_INTERVAL = 23;
 var instance;
 var Queue = {
   getInstance: function () {
@@ -160,7 +166,7 @@ QueueThread.prototype.run = function (firenow) {
     setTimeout(
       function () {
         run(true);
-      }, 
+      },
       MILSEC_INTERVAL
     );
   }
