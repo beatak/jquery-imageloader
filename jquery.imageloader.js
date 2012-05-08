bf6b81498a0028e8d6e95b11dffff9d598f89d9a
Takashi Mizohata <beatak@gmail.com>
2012-05-08 09:22:59 -0400

    doc change

--- jquery.imageloader.js
+++ jquery.imageloader.js
@@ -12,8 +12,8 @@ var DEFAULT_OPTIONS = {
 /**
  * jQuery.imageloader() lets you load images lazily.  It is designed
  * to be memory less intensive, cleaning up all after done and using
- * a singleton-style queue mechanism so that it won't overwhelm 
- * browser UI thread.
+ * a singleton queue mechanism so that it won't overwhelm browser UI
+ * thread.
  * 
  * todo: maybe count seconds and if still not loading all, should
  * fire error?
